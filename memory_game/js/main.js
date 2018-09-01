606d9468602cb2c784a3da79a8096b395e198180
Colin <colinmdaniel@gamil.com>
2018-08-31 19:20:19 -0500

    javaScript conditional statments added

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,31 @@
 console.log("Up and running!")
 
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
+var cards = ["Queen","Queen","King","King"];
+var cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+		var cardOne = cards[0];
+		cardsInPlay.push("cardOne");
+
+		var cardTwo = cards[1];
+		cardsInPlay.push("cardTwo");
+	
+		var cardThree = cards[2];
+		cardsInPlay.push("cardThree");
+
+		var cardFour = cards[3];
+		cardsInPlay.push("cardFour");
+	
+
+
+for(i = 0; i < cardsInPlay.length;i++){
+  console.log("User fliped " + cards[i])
+};
+
+if (cardsInPlay.length ===2 && (cards[0] === cards[1]|| cards[2]=== cards [3])){
+  alert("Congrats you found a match");
+}
+
+else{
+    alert("Try Again!");
+    }
