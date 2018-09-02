f45b8bff8704efd6df168fce392c884e9226737e
Colin <colinmdaniel@gamil.com>
2018-09-01 21:06:01 -0500

    added objects instead of strings to cards

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,25 @@
 console.log("Up and running!")
 
-var cards = ["Queen","Queen","King","King"];
+var cards = [{
+rank:"Queen",
+suit:"Hearts",
+cardImage:"images/queen-of-hearts.png"
+},
+{rank:"Queen",
+suit:"Diamonds",
+cardImage:"images/queen-of-diamonds.png"
+},
+{
+ rank:"King",
+ suit:"Hearts",
+ cardImage:"images/king-of-hearts.png"
+},
+{
+ rank:"King",
+ suit:"Diamonds",
+ cardImage:"images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 
 
@@ -19,8 +38,10 @@ var checkForMatch = () => {
 
 var flipcard = function(cardId){
 	
-	console.log("User flipped" + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped" + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit)
 	checkForMatch();
 
   if(cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
