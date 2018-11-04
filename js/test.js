32c0f9624e6880bb38f7a2a7d77f175409732955
Colin Daniel <colinmdaniel@gmail.com>
2018-11-03 21:44:51 -0500

    file within file stoping deployment

---
+++ js/test.js
@@ -0,0 +1,25 @@
+console.log("Up and running!");
+
+var cards = [{
+rank:"Queen",
+suit:"Hearts",
+cardImage:"images/images/queen-of-hearts.png"
+},
+{rank:"Queen",
+suit:"Diamonds",
+cardImage:"images/images/queen-of-diamonds.png"
+},
+{
+ rank:"King",
+ suit:"Hearts",
+ cardImage:"images/images/king-of-hearts.png"
+},
+{
+ rank:"King",
+ suit:"Diamonds",
+ cardImage:"images/images/king-of-diamonds.png"
+}
+];
+
+let cardDrawn = false;
+let card1 = 
